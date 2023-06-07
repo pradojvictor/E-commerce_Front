d887af3eec146ec99e914fca7127f8b9160b1ecb
pradojvictor <joaovictorred@hotmail.com>
2023-06-06 22:18:14 -0300

    add context to cart to be used by others components and pages

---
+++ components/CartContext.js
@@ -0,0 +1,30 @@
+const { createContext, useState, useEffect } = require("react");
+
+export const CartContext = createContext({});
+
+export function CartContextProvider({ children }) {
+
+    const ls = typeof window !== "undefined" ? window.localStorage : null;
+    const [cartProducts, setCartProducts] = useState([]);
+    useEffect(() => {
+        if (cartProducts?.length > 0) {
+            ls?.setItem('cart', JSON.stringify(cartProducts));
+        }
+    }, [cartProducts]);
+    useEffect(() => {
+        if (ls && ls.getItem('cart')) {
+            setCartProducts(JSON.parse(ls.getItem('cart')));
+        }
+    }, []);
+
+    function addProduct(productId) {
+        setCartProducts(prev => [...prev, productId]);
+    }
+    return (
+        <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+//8:29:56
