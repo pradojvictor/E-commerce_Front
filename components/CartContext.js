6b33587c18113cf58b87ea9dec23ec02f41665b5
pradojvictor <joaovictorred@hotmail.com>
2023-06-07 15:44:33 -0300

    chargen: add function remove/add itens for cart

--- components/CartContext.js
+++ components/CartContext.js
@@ -10,21 +10,31 @@ export function CartContextProvider({ children }) {
         if (cartProducts?.length > 0) {
             ls?.setItem('cart', JSON.stringify(cartProducts));
         }
-    }, [cartProducts]);
+    }, [cartProducts, ls]);
     useEffect(() => {
         if (ls && ls.getItem('cart')) {
             setCartProducts(JSON.parse(ls.getItem('cart')));
         }
-    }, []);
+    }, [ls]);
 
     function addProduct(productId) {
         setCartProducts(prev => [...prev, productId]);
     }
+    function removeProduct(productId) {
+        setCartProducts(prev => {
+            const position = prev.indexOf(productId);
+            if (position !== -1) {
+                return prev.filter((value, index) => index !== position);
+            }
+            return prev;
+        });
+    }
     return (
-        <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct }}>
+        <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct, removeProduct }}>
             {children}
         </CartContext.Provider>
     );
 }
 
-//8:29:56
+//9:02:40 filter()
+//add ls in [ls]
