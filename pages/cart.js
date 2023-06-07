6453cd0519daa21583edab644f3f5b003195bcc3
pradojvictor <joaovictorred@hotmail.com>
2023-06-07 15:40:28 -0300

    add: page cart, and freature in cart

---
+++ pages/cart.js
@@ -0,0 +1,128 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-key */
+import Button from "@/components/Button";
+import { CartContext } from "@/components/CartContext";
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import Table from "@/components/Table";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+const Columnswrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1.2fr .8fr;
+    gap: 2.5rem;
+    margin-top: 2.5rem;
+`;
+const Box = styled.div`
+    background-color: #fff;
+    border-radius: 0.625rem;
+    padding: 1.875rem;
+`;
+
+const ProductInfoCell = styled.td`
+    padding: 0.625rem 0;
+`;
+const ProductImageBox = styled.div`
+    width: 6.25rem;
+    height: 6.25rem;
+    padding: 0.625rem;
+    border: 0.063rem solid rgba(0,0,0,.1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 0.625rem;
+    img{
+        max-width: 6rem;
+        max-height: 6rem;
+    }
+`;
+const QuantityLabel = styled.span`
+    padding: 0 0.188rem;
+`;
+
+export default function CartPage() {
+    const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        if (cartProducts.length > 0) {
+            axios.post('/api/cart', { ids: cartProducts })
+                .then(response => {
+                    setProducts(response.data);
+                })
+        }
+    }, [cartProducts]);
+    function moreOfThisProduct(id) {
+        addProduct(id);
+    }
+    function lessOfThisProduct(id) {
+        removeProduct(id);
+    }
+    let total = 0;
+    for (const productId of cartProducts) {
+        const price = products.find(p => p._id === productId)?.price || 0;
+        total += price;
+    }
+    return (
+        <>
+            <Header />
+            <Center>
+                <Columnswrapper>
+                    <Box>
+                        <h2>Cart</h2>
+                        {!cartProducts?.length && (
+                            <div>Your cart is empty</div>
+                        )}
+                        {products?.length > 0 && (
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>Product</th>
+                                        <th>Quanity</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {products.map(product => (
+                                        <tr>
+                                            <ProductInfoCell>
+                                                <ProductImageBox>
+                                                    <img src={product.images[0]} alt="" />
+                                                </ProductImageBox>
+                                                {product.title}
+                                            </ProductInfoCell>
+                                            <td>
+                                                <Button onClick={() => lessOfThisProduct(product._id)}>-</Button>
+                                                <QuantityLabel>
+                                                    {cartProducts.filter(id => id === product._id).length}
+                                                </QuantityLabel>
+                                                <Button onClick={() => moreOfThisProduct(product._id)}>+</Button>
+                                            </td>
+                                            <td>${cartProducts.filter(id => id === product._id).length * product.price}</td>
+                                        </tr>
+                                    ))}
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td>${total}</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        )}
+                    </Box>
+                    {!!cartProducts?.length && (
+                        <Box>
+                            <h2>Order information</h2>
+                            <input type="text" placeholder="Address" />
+                            <input type="text" placeholder="Address 2" />
+                            <Button block black>Continue to payment</Button>
+                        </Box>
+                    )}
+                </Columnswrapper>
+            </Center>
+        </>
+    )
+}
+
+//8:38:13
