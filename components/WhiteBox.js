740ee38a936d4a56ea9d2a1488a5171988ee4447
pradojvictor <joaovictorred@hotmail.com>
2023-07-19 02:25:43 -0300

    feature: component box image from page product

---
+++ components/WhiteBox.js
@@ -0,0 +1,8 @@
+const { styled } = require("styled-components");
+
+const WhiteBox = styled.div`
+    background-color: #fff;
+    border-radius: 0.625rem;
+    padding: 1.875rem;
+`;
+export default WhiteBox;
